91aa98e18b797fc6857906d0e29e937f60a07890
Vanessa Virgitti <virgitti@gmail.com>
2020-07-16 12:24:41 +0100

    replaced magic strings by const

--- go/gilded-rose.go
+++ go/gilded-rose.go
@@ -6,16 +6,21 @@ type Item struct {
 }
 
 func UpdateQuality(items []*Item) {
+	agedBrie := "Aged Brie"
+	backstage := "Backstage passes to a TAFKAL80ETC concert"
+	sulfuras := "Sulfuras, Hand of Ragnaros"
+
 	for i := 0; i < len(items); i++ {
 
-		if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-			if items[i].quality > 0 && items[i].name != "Sulfuras, Hand of Ragnaros" {
+		if items[i].name != agedBrie && items[i].name !=
+			backstage{
+			if items[i].quality > 0 && items[i].name != sulfuras {
 				items[i].quality = items[i].quality - 1
 			}
 		} else {
 			if items[i].quality < 50 {
 				items[i].quality = items[i].quality + 1
-				if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
+				if items[i].name == backstage {
 					if items[i].sellIn < 11 {
 						if items[i].quality < 50 {
 							items[i].quality = items[i].quality + 1
@@ -30,15 +35,15 @@ func UpdateQuality(items []*Item) {
 			}
 		}
 
-		if items[i].name != "Sulfuras, Hand of Ragnaros" {
+		if items[i].name != sulfuras {
 			items[i].sellIn = items[i].sellIn - 1
 		}
 
 		if items[i].sellIn < 0 {
-			if items[i].name != "Aged Brie" {
-				if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
+			if items[i].name != agedBrie {
+				if items[i].name != backstage {
 					if items[i].quality > 0 {
-						if items[i].name != "Sulfuras, Hand of Ragnaros" {
+						if items[i].name != sulfuras {
 							items[i].quality = items[i].quality - 1
 						}
 					}
