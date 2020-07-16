55efc2b4f2ea9185a7ca508dc405974408e312bb
Vanessa Virgitti <virgitti@gmail.com>
2020-07-16 14:31:34 +0100

    Refactor sulfuras name statement into a func

--- go/gilded-rose.go
+++ go/gilded-rose.go
@@ -17,12 +17,16 @@ func isBackstage(itemName string) bool {
 	return itemName == backstage
 }
 
+func isSulfuras(itemName string) bool {
+	return itemName == sulfuras
+}
+
 func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
 		itemName := items[i].name
 
 		if !isAgedBrie(itemName) && !isBackstage(itemName) {
-			if items[i].quality > 0 && itemName != sulfuras {
+			if items[i].quality > 0 && !isSulfuras(itemName) {
 				items[i].quality = items[i].quality - 1
 			}
 		} else {
@@ -37,13 +41,13 @@ func UpdateQuality(items []*Item) {
 			}
 		}
 
-		if itemName != sulfuras {
+		if !isSulfuras(itemName) {
 			items[i].sellIn = items[i].sellIn - 1
 		}
 
 		if items[i].sellIn < 0 {
 			if !isAgedBrie(itemName) {
-				if !isBackstage(itemName) && items[i].quality > 0 && itemName != sulfuras {
+				if !isBackstage(itemName) && items[i].quality > 0 && !isSulfuras(itemName) {
 					items[i].quality = items[i].quality - 1
 				} else {
 					items[i].quality = items[i].quality - items[i].quality
