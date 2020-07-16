be187d04e06a4199880db9807e44230511094e17
Vanessa Virgitti <virgitti@gmail.com>
2020-07-16 15:42:19 +0100

    Remove an if statement

--- go/gilded-rose.go
+++ go/gilded-rose.go
@@ -39,20 +39,28 @@ func decreaseSellin(item *Item) {
 	}
 }
 
+func isLowerThan50(value int) bool {
+	return value < 50
+}
+
+func isNotSpecialItem(itemName string) bool {
+	return !isAgedBrie(itemName) && !isBackstage(itemName) && !isSulfuras(itemName)
+}
+
 func UpdateQuality(items []*Item) {
 	for i, item := range items {
 		itemName := item.name
 
-		if !isAgedBrie(itemName) && !isBackstage(itemName) {
-			if hasPositiveQuality(items[i].quality) && !isSulfuras(itemName) {
-				items[i].quality =  decrease(items[i].quality)
-			}
-		} else {
-			if items[i].quality < 50 {
-				items[i].quality =  increase(items[i].quality)
-				if isBackstage(itemName) && items[i].sellIn < 11 && items[i].quality < 50 {
+		if isNotSpecialItem(itemName) && hasPositiveQuality(items[i].quality) {
+			items[i].quality = decrease(items[i].quality)
+		}
+
+		if isAgedBrie(itemName) || isBackstage(itemName) {
+			if isLowerThan50(items[i].quality) {
+				items[i].quality = increase(items[i].quality)
+				if isBackstage(itemName) && items[i].sellIn < 11 && isLowerThan50(items[i].quality) {
 					items[i].quality = increase(items[i].quality)
-					if items[i].sellIn < 6 && items[i].quality < 50 {
+					if items[i].sellIn < 6 && isLowerThan50(items[i].quality) {
 						items[i].quality = increase(items[i].quality)
 					}
 				}
@@ -61,16 +69,14 @@ func UpdateQuality(items []*Item) {
 
 		decreaseSellin(items[i])
 
-
-
 		if items[i].sellIn < 0 {
-			if isAgedBrie(itemName) && items[i].quality < 50 {
-				items[i].quality =  increase(items[i].quality)
+			if isAgedBrie(itemName) && isLowerThan50(items[i].quality) {
+				items[i].quality = increase(items[i].quality)
 			}
 
 			if !isAgedBrie(itemName) {
-				if !isBackstage(itemName) &&  hasPositiveQuality(items[i].quality) && !isSulfuras(itemName) {
-					items[i].quality =  decrease(items[i].quality)
+				if !isBackstage(itemName) && hasPositiveQuality(items[i].quality) && !isSulfuras(itemName) {
+					items[i].quality = decrease(items[i].quality)
 				} else {
 					items[i].quality = items[i].quality - items[i].quality
 				}
