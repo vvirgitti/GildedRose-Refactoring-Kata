2d14baf388018bc047066a69cb7e1adeea8fe7e5
Vanessa Virgitti <virgitti@gmail.com>
2020-07-16 12:14:21 +0100

    Simplify if statement

--- go/gilded-rose.go
+++ go/gilded-rose.go
@@ -9,10 +9,8 @@ func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
 
 		if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-			if items[i].quality > 0 {
-				if items[i].name != "Sulfuras, Hand of Ragnaros" {
-					items[i].quality = items[i].quality - 1
-				}
+			if items[i].quality > 0 && items[i].name != "Sulfuras, Hand of Ragnaros" {
+				items[i].quality = items[i].quality - 1
 			}
 		} else {
 			if items[i].quality < 50 {
