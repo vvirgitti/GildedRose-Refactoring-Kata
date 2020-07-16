8302038ec8e0723b6ad9159bb404c5d5ccd5b479
Vanessa Virgitti <virgitti@gmail.com>
2020-07-16 14:12:22 +0100

    Refactor items name into a variable

--- go/gilded-rose.go
+++ go/gilded-rose.go
@@ -9,20 +9,22 @@ const agedBrie = "Aged Brie"
 const backstage = "Backstage passes to a TAFKAL80ETC concert"
 const sulfuras = "Sulfuras, Hand of Ragnaros"
 
+func isAgedBrie(itemName string) bool {
+	return itemName == agedBrie
+}
 
 func UpdateQuality(items []*Item) {
-
-
 	for i := 0; i < len(items); i++ {
+		itemName := items[i].name
 
-		if items[i].name != agedBrie && items[i].name != backstage {
-			if items[i].quality > 0 && items[i].name != sulfuras {
+		if !isAgedBrie(itemName) && itemName != backstage {
+			if items[i].quality > 0 && itemName != sulfuras {
 				items[i].quality = items[i].quality - 1
 			}
 		} else {
 			if items[i].quality < 50 {
 				items[i].quality = items[i].quality + 1
-				if items[i].name == backstage && items[i].sellIn < 11 && items[i].quality < 50 {
+				if itemName == backstage && items[i].sellIn < 11 && items[i].quality < 50 {
 					items[i].quality = items[i].quality + 1
 					if items[i].sellIn < 6 && items[i].quality < 50 {
 						items[i].quality = items[i].quality + 1
@@ -31,13 +33,13 @@ func UpdateQuality(items []*Item) {
 			}
 		}
 
-		if items[i].name != sulfuras {
+		if itemName != sulfuras {
 			items[i].sellIn = items[i].sellIn - 1
 		}
 
 		if items[i].sellIn < 0 {
-			if items[i].name != agedBrie {
-				if items[i].name != backstage && items[i].quality > 0 && items[i].name != sulfuras {
+			if itemName != agedBrie {
+				if itemName != backstage && items[i].quality > 0 && itemName != sulfuras {
 					items[i].quality = items[i].quality - 1
 				} else {
 					items[i].quality = items[i].quality - items[i].quality
