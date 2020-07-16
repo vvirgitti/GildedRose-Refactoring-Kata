01510f0518b0b40b23c75749c9f5e76ad8d66c95
Vanessa Virgitti <virgitti@gmail.com>
2020-07-16 14:30:22 +0100

    Refactor backstage name statement into a func

--- go/gilded-rose.go
+++ go/gilded-rose.go
@@ -13,18 +13,22 @@ func isAgedBrie(itemName string) bool {
 	return itemName == agedBrie
 }
 
+func isBackstage(itemName string) bool {
+	return itemName == backstage
+}
+
 func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
 		itemName := items[i].name
 
-		if !isAgedBrie(itemName) && itemName != backstage {
+		if !isAgedBrie(itemName) && !isBackstage(itemName) {
 			if items[i].quality > 0 && itemName != sulfuras {
 				items[i].quality = items[i].quality - 1
 			}
 		} else {
 			if items[i].quality < 50 {
 				items[i].quality = items[i].quality + 1
-				if itemName == backstage && items[i].sellIn < 11 && items[i].quality < 50 {
+				if isBackstage(itemName) && items[i].sellIn < 11 && items[i].quality < 50 {
 					items[i].quality = items[i].quality + 1
 					if items[i].sellIn < 6 && items[i].quality < 50 {
 						items[i].quality = items[i].quality + 1
@@ -38,8 +42,8 @@ func UpdateQuality(items []*Item) {
 		}
 
 		if items[i].sellIn < 0 {
-			if itemName != agedBrie {
-				if itemName != backstage && items[i].quality > 0 && itemName != sulfuras {
+			if !isAgedBrie(itemName) {
+				if !isBackstage(itemName) && items[i].quality > 0 && itemName != sulfuras {
 					items[i].quality = items[i].quality - 1
 				} else {
 					items[i].quality = items[i].quality - items[i].quality
