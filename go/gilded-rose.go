19b040caa9dc8c578f6aaaf8a4bf67b6a9146930
Vanessa Virgitti <virgitti@gmail.com>
2020-07-16 12:32:28 +0100

    Refactor some if statements

--- go/gilded-rose.go
+++ go/gilded-rose.go
@@ -5,6 +5,10 @@ type Item struct {
 	sellIn, quality int
 }
 
+//func IsSpecialItem(itemName string) bool {
+//
+//}
+
 func UpdateQuality(items []*Item) {
 	agedBrie := "Aged Brie"
 	backstage := "Backstage passes to a TAFKAL80ETC concert"
@@ -12,24 +16,17 @@ func UpdateQuality(items []*Item) {
 
 	for i := 0; i < len(items); i++ {
 
-		if items[i].name != agedBrie && items[i].name !=
-			backstage{
+		if items[i].name != agedBrie && items[i].name != backstage {
 			if items[i].quality > 0 && items[i].name != sulfuras {
 				items[i].quality = items[i].quality - 1
 			}
 		} else {
 			if items[i].quality < 50 {
 				items[i].quality = items[i].quality + 1
-				if items[i].name == backstage {
-					if items[i].sellIn < 11 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-					if items[i].sellIn < 6 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
+				if items[i].name == backstage && items[i].sellIn < 11 && items[i].quality < 50 {
+					items[i].quality = items[i].quality + 1
+					if items[i].sellIn < 6 && items[i].quality < 50 {
+						items[i].quality = items[i].quality + 1
 					}
 				}
 			}
@@ -42,10 +39,8 @@ func UpdateQuality(items []*Item) {
 		if items[i].sellIn < 0 {
 			if items[i].name != agedBrie {
 				if items[i].name != backstage {
-					if items[i].quality > 0 {
-						if items[i].name != sulfuras {
-							items[i].quality = items[i].quality - 1
-						}
+					if items[i].quality > 0 && items[i].name != sulfuras {
+						items[i].quality = items[i].quality - 1
 					}
 				} else {
 					items[i].quality = items[i].quality - items[i].quality
@@ -57,5 +52,4 @@ func UpdateQuality(items []*Item) {
 			}
 		}
 	}
-
 }
